6a90d4240f320cc622893e163b5d8f457dbe9556
Calvin Min <Calvinjmin@gmail.com>
2024-10-05 09:09:21 -0400

    Fix Static Path

--- src/content/ScribblePosts.js
+++ src/content/ScribblePosts.js
@@ -3,7 +3,7 @@ export const scribblePosts = [
     title: "Bread and Butter",
     excerpt:
       "Given my year and change of expertise, here are my go-to Boston restaurants",
-    contentPath: `${process.env.PUBLIC_URL}/scribbles/bread_and_butter.md`,
+    contentPath: "/scribbles/bread_and_butter.md",
     publicationDate: "10/5/2024",
   },
 ];
